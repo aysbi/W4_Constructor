97e6cff816b27a5922c08ece2cc382bb714dbb5d
Aysel Bilmez <aybilmez@gmail.com>
2024-10-23 09:08:43 +0300

    Proje dosyası ekle.

---
+++ W4_Constructor/Bebek.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W4P2_Constructor
+{
+    public class Bebek
+    {
+        public string Name; //alanlari olusturuyoruz
+        public string Surname;
+        public DateTime Birthday;
+
+        public Bebek() //ici bos constructor
+        { }
+
+        public Bebek(string name, string surname) //sadece ad soyad alan bir constructor dogum tarihi icerisinde o anin dogum gunu olarak veriliyor
+        {
+            Name = name;
+            Surname = surname;
+            Birthday = DateTime.Now;
+            Console.WriteLine("Ingaaaa");
+        }
+
+
+
+        public void PrintInfo() //konsola yazdirma icin bir metot
+        {
+            Console.WriteLine($"{Name} {Surname} adinda dogan bebegin dogum tarihi {Birthday}.");
+        }
+
+    }
+}
---
+++ W4_Constructor/Program.cs
@@ -0,0 +1,16 @@
+﻿using W4P2_Constructor;
+
+Bebek bebek1 = new Bebek(); //ici bos constructori yaparsak asagidaki gibi tek tek tanimlamamiz gerekir
+bebek1.Name = "Esra";
+bebek1.Surname = "Suluoglu";
+bebek1.Birthday = new DateTime(1990, 01, 01);
+
+bebek1.PrintInfo();
+
+Console.WriteLine("---------------------------");
+
+Bebek bebek2 = new Bebek("aysel", "bilmez"); //ici dolu constructor yazarsak tek satirda isi halledebiliriz
+
+bebek2.PrintInfo();
+
+Console.ReadKey();
